71f0598796085162557709bc88f90a8bd87ebe90
Computer Carl <carl@computercarl.com>
2019-08-19 18:57:22 -0400

    progressing

--- src/components/resume-form.jsx
+++ src/components/resume-form.jsx
@@ -23,9 +23,30 @@ const MauticForm = styled.div`
     }
 
     .mauticform-button {
-        background-color: green !important;
+      box-shadow: 0px 0px 0px 2px #9fb4f2;
+      background-color:#7892c2;
+      border-radius:10px;
+      border:1px solid #4e6096;
+      display:inline-block;
+      cursor:pointer;
+      color:#ffffff;
+      font-family:Arial;
+      font-size:19px;
+      padding:12px 37px;
+      text-decoration:none;
+      text-shadow:0px 1px 0px #283966;
+    }
+    .mauticform-button:active {
+      position:relative;
+      top:1px;
+    }
+    .mauticform-button:hover {
+      color: white;
+      background-color: #476e9e;
     }
 `
+
+
 
 export default () => {
     useEffect(() => {
