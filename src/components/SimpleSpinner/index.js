48b0c2a956c62da41b96d22a064352f0e8b186b2
Computer Carl <carl@computercarl.com>
2020-07-13 11:41:33 +0800

    added some loaders

---
+++ src/components/SimpleSpinner/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Spinner = () => (
+  <StyledSpinner viewBox="0 0 50 50">
+    <circle
+      className="path"
+      cx="25"
+      cy="25"
+      r="20"
+      fill="none"
+      strokeWidth="1"
+    />
+  </StyledSpinner>
+);
+
+const StyledSpinner = styled.svg`
+    position: relative;
+    transform: translateX(-50%);
+    animation: 2s linear infinite;
+    margin: 25px auto;
+    left: 50%;
+    width: 50px;
+    height: 50px;
+  
+  & .path {
+    stroke: #5652BF;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+  
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+`;
+
+export default Spinner;
